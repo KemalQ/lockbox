c9f8247e62311c5681ecd8fe934c95e9dc605838
KemalQ <292126@ogr.ktu.edu.tr>
2025-06-01 23:36:57 +0300

    MethodEmbedding: -embeddingMethodOne refactored -extractingMethodOne refactored

--- ImageProcessingService/src/main/java/lockBox/Service/embedingMethods/MethodEmbedding.java
+++ ImageProcessingService/src/main/java/lockBox/Service/embedingMethods/MethodEmbedding.java
@@ -3,15 +3,15 @@ package lockBox.Service.embedingMethods;
 import lockBox.Service.impl.ImageProcessingImpl;
 import lockBox.Service.impl.KohJao;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Data
 @Service
 public class MethodEmbedding {
-
-
     public MethodEmbedding() {}
 
     public List<double[][]> embeddingMethodOne(List<double[][]> arrayOfBlocks, String text){
@@ -21,80 +21,139 @@ public class MethodEmbedding {
         String binaryMessage = toBinary(text);
         String binaryLength = String.format("%10s", Integer.toBinaryString(binaryMessage.length())).replace(' ', '0');
 
-        for (int repeat = 0; repeat < 3; repeat++) {
-            int baseIndex = repeat * (3 + 10 + binaryMessage.length());
-
-            // 1. Внедряем флаг "сообщение есть"
-            for (int i = 0; i < 3; i++) {
-                arrayOfBlocks.set(baseIndex+i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + i), true));
-                //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + i), true); // вставляем 1
-            }
-
-            // 2. Внедряем длину сообщения (в битах)
-            for (int i = 0; i < 10; i++) {
-                int bit = Character.getNumericValue(binaryLength.charAt(i));
-                if (bit==0){
-                    arrayOfBlocks.set(baseIndex + 3 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), false));
-                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), false);
-                }
-                else arrayOfBlocks.set(baseIndex + 3 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), true));
-                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), true);
-            }
-
-            // 3. Внедряем само сообщение
-            for (int i = 0; i < binaryMessage.length(); i++) {
-                int bit = Character.getNumericValue(binaryMessage.charAt(i));
-                if (bit==0){
-                    arrayOfBlocks.set(baseIndex + 3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), false));
-                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), false);
-                }
-                else arrayOfBlocks.set(baseIndex + 3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), true));
-                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), true);
-            }
+        // 1. флаг наличия сообщения
+        for (int i = 0; i < 3; i++){
+            arrayOfBlocks.set(i, kohJao.embedBitInBlock(arrayOfBlocks.get(i), true));
         }
+        // 2. Внедряю длину сообщения (в битах)
+        for (int i = 0; i < 10; i++) {
+            int bit = Character.getNumericValue(binaryLength.charAt(i));
+            if (bit==0){
+                arrayOfBlocks.set(3 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(3 + i), false));//starts from 3 block
+            }
+            else arrayOfBlocks.set(i, kohJao.embedBitInBlock(arrayOfBlocks.get(3 + i), true));//starts from 3 block
+        }
+
+        // 3. Внедряем само сообщение
+        for (int i = 0; i < binaryMessage.length(); i++) {
+            int bit = Character.getNumericValue(binaryMessage.charAt(i));
+            if (bit==0){
+                arrayOfBlocks.set(3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(3 + 10 + i), false));
+            }
+            else arrayOfBlocks.set(3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(3 + 10 + i), true));
+        }
+
         return arrayOfBlocks;
+
+
+//        for (int repeat = 0; repeat < 3; repeat++) {
+//            int baseIndex = repeat * (3 + 10 + binaryMessage.length());
+//
+//            // 1. Внедряем флаг "сообщение есть"
+//            for (int i = 0; i < 3; i++) {
+//                arrayOfBlocks.set(baseIndex+i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + i), true));
+//                //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + i), true); // вставляем 1
+//            }
+//
+//            // 2. Внедряем длину сообщения (в битах)
+//            for (int i = 0; i < 10; i++) {
+//                int bit = Character.getNumericValue(binaryLength.charAt(i));
+//                if (bit==0){
+//                    arrayOfBlocks.set(baseIndex + 3 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), false));
+//                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), false);
+//                }
+//                else arrayOfBlocks.set(baseIndex + 3 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), true));
+//                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + i), true);
+//            }
+//
+//            // 3. Внедряем само сообщение
+//            for (int i = 0; i < binaryMessage.length(); i++) {
+//                int bit = Character.getNumericValue(binaryMessage.charAt(i));
+//                if (bit==0){
+//                    arrayOfBlocks.set(baseIndex + 3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), false));
+//                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), false);
+//                }
+//                else arrayOfBlocks.set(baseIndex + 3 + 10 + i, kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), true));
+//                    //kohJao.embedBitInBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i), true);
+//            }
+//        }
+//        return arrayOfBlocks;
     }
 
     public String extractingMethodOne(List<double[][]> arrayOfBlocks) {
         KohJao kohJao = new KohJao();
 
-        for (int repeat = 0; repeat < 3; repeat++) {
-            int baseIndex = repeat * (3 + 10 + 512); //пока max длина сообщения 512 бит, можно позже вычислять динамически
-
-            //1. Чтение флага "сообщение есть"
-            int trueCount = 0;
-            for (int i = 0; i < 3; i++) {
-                Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + i));
-                if (bit != null && bit) {
-                    trueCount++;
-                }
-            }
-
-            if (trueCount >= 2) { // флаг подтверждён
-                //2. Чтение длины
-                StringBuilder lengthBinary = new StringBuilder();
-                for (int i = 0; i < 10; i++) {
-                    Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + 3 + i));
-                    if (bit == null) return null;
-                    lengthBinary.append(bit ? '1' : '0');
-                }
-
-                int messageLength = Integer.parseInt(lengthBinary.toString(), 2);//получил длину внедренного сообщения в int
-
-                // 3. Чтение сообщения
-                StringBuilder binaryMessage = new StringBuilder();
-                for (int i = 0; i < messageLength; i++) {
-                    Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i));
-                    if (bit == null) return null;
-                    binaryMessage.append(bit ? '1' : '0');
-                }
-
-                // Расшифровка бинарного текста в строку
-                return fromBinary(binaryMessage.toString());
+        //1. Чтение флага "сообщение есть"
+        int trueCount = 0;
+        for (int i = 0; i < 3; i++) {
+            Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(i));
+            if (bit != null && bit) {
+                trueCount++;
             }
         }
 
-        return null; // сообщение не найдено
+        log.info("MethodEmbedding - extractingMethodOne - trueCount: " + trueCount);//TODO delete after checking 01.06.2025
+        if (trueCount >= 2){// флаг подтверждён
+            //2. Чтение длины
+            StringBuilder lengthBinary = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(3 + i));
+                if (bit == null) return null;
+                lengthBinary.append(bit ? '1' : '0');
+            }
+            int messageLength = Integer.parseInt(lengthBinary.toString(), 2);// получил длину внедренного сообщения в int
+            log.info("MethodEmbedding - extractingMethodOne - messageLength: " + messageLength);//TODO delete after checking 01.06.2025
+
+            // 3. Чтение сообщения
+            StringBuilder binaryMessage = new StringBuilder();
+            for (int i = 0; i < messageLength; i++) {
+                Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(3 + 10 + i));
+                if (bit == null) return null;
+                binaryMessage.append(bit ? '1' : '0');
+            }
+            log.info("MethodEmbedding - extractingMethodOne - fromBinaryToStringMessage: " + fromBinary(binaryMessage.toString()));//TODO delete after checking 01.06.2025
+            // Расшифровка бинарного текста в строку
+            return fromBinary(binaryMessage.toString());
+        }
+        else return null;
+
+
+//        for (int repeat = 0; repeat < 3; repeat++) {
+//            int baseIndex = repeat * (3 + 10 + 512); //пока max длина сообщения 512 бит, можно позже вычислять динамически
+//
+//            //1. Чтение флага "сообщение есть"
+//            int trueCount = 0;
+//            for (int i = 0; i < 3; i++) {
+//                Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + i));
+//                if (bit != null && bit) {
+//                    trueCount++;
+//                }
+//            }
+//
+//            if (trueCount >= 2) { // флаг подтверждён
+//                //2. Чтение длины
+//                StringBuilder lengthBinary = new StringBuilder();
+//                for (int i = 0; i < 10; i++) {
+//                    Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + 3 + i));
+//                    if (bit == null) return null;
+//                    lengthBinary.append(bit ? '1' : '0');
+//                }
+//
+//                int messageLength = Integer.parseInt(lengthBinary.toString(), 2);//получил длину внедренного сообщения в int
+//
+//                // 3. Чтение сообщения
+//                StringBuilder binaryMessage = new StringBuilder();
+//                for (int i = 0; i < messageLength; i++) {
+//                    Boolean bit = kohJao.extractBitFromBlock(arrayOfBlocks.get(baseIndex + 3 + 10 + i));
+//                    if (bit == null) return null;
+//                    binaryMessage.append(bit ? '1' : '0');
+//                }
+//
+//                // Расшифровка бинарного текста в строку
+//                return fromBinary(binaryMessage.toString());
+//            }
+//        }
+//        return null; // сообщение не найдено
     }
 
 
---
+++ ImageProcessingService/src/test/java/lockBox/Service/embeddingMethodsTest/MethodEmbeddingTest.java
@@ -0,0 +1,41 @@
+package lockBox.Service.embeddingMethodsTest;
+
+import lockBox.Service.embedingMethods.MethodEmbedding;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MethodEmbeddingTest {
+
+    @Test
+    public void testEmbeddingAndExtracting() {
+        MethodEmbedding method = new MethodEmbedding();
+        String originalMessage = "hello";
+
+        // 1. Генерируем пустые блоки
+        int totalBlocks = 3 * (3 + 10 + originalMessage.length() * 8); // умножаем на 8 т.к. в битах
+        List<double[][]> blocks = new ArrayList<>();
+        for (int i = 0; i < totalBlocks; i++) {
+            blocks.add(generateEmptyBlock()); // создаём пустой блок 8x8
+        }
+
+        // 2. Внедрение
+        List<double[][]> embeddedBlocks = method.embeddingMethodOne(blocks, originalMessage);
+
+        // 3. Извлечение
+        String extractedMessage = method.extractingMethodOne(embeddedBlocks);
+
+        // 4. Проверка
+        assertEquals(originalMessage, extractedMessage);
+    }
+
+    private double[][] generateEmptyBlock() {
+        double[][] block = new double[8][8];
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                block[i][j] = Math.random(); // или 0.0, если нужны предсказуемые значения
+        return block;
+    }
+}
